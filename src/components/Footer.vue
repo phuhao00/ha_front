716ba775eb41e477b6b291ecf0c0f08c6d847537
huhao <1559531623@qq.com>
2019-03-29 11:30:34 +0800

    修改 了home footer的大概轮廓

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
-      <footer class="footer fixed-bottom">
-        <div class="container">
-          hhhhhhhhhhhhhhhhh
+    <div class="Home_Footer">
+      <footer class="footer " >
+        <div class="container ">
+        <div class="row">
+          <div class="col-xs-4 col-md-2 col-sm-3 Home_footer__list-item">
+            <h3 class="Home_Footer__Title">Help</h3>
+            <ul class="Home_footer__list">
+              <li class="Home_footer__list-item">
+                <a data-track="true" data-track-location="footer" class="Home_Footer__list-link" href="#">Knowledge Base</a>
+              </li>
+              <li class="Home_footer__list-item">
+                <a data-track="true" data-track-location="footer" class="Home_Footer__list-link"  href="#">Forum</a>
+              </li>
+            </ul>
+          </div>
         </div>
+      </div>
       </footer>
     </div>
 </template>
-
 <script>
 export default {
   name: 'Footer'
@@ -15,5 +26,23 @@ export default {
 </script>
 
 <style scoped>
+  .Home_Footer{
+    /*font-weight: 400;*/
+    text-align: center;
+    padding: 20px;
+    background-color:skyblue;
+    margin-top:200px
+  }
+  .Home_Footer__Title{
 
+  }
+  .Home_Footer__list-link{
+
+  }
+  .Home_footer__list-item{
+
+  }
+  .Home_footer__list{
+
+  }
 </style>
